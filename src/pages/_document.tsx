5c9c20e7eee02edaff7f3a0a2cb672e282ec8871
Thalles Ian <thallesyam@users.=.github.com>
2021-05-28 16:43:52 -0300

    add dinamic title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Ig News</title>
-
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Hello <span>World</span>
-    </h1>
+    <>
+      <Head>
+        <title>Inicio | Ig.news</title>
+      </Head>
+
+      <h1>
+        Hello <span>World</span>
+      </h1>
+    </>
   )
 }
