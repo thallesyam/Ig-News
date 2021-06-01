9edcccbe08e646432df0bc52922bcf9d831a174b
Thalles Ian <thallesyam@users.=.github.com>
2021-05-31 21:12:03 -0300

    feat: create subscribe button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
 import styles from './home.module.scss'
+import { SubscribeButton } from '../components/SubscribeButton/index'
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
             Get access to all the publications <br />
             <span>for $ 9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
