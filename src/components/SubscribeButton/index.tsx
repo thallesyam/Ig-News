1655c903f1ddd9167d6fa6cc4f5ce961b6f19d25
Thalles Ian <thallesyam@users.=.github.com>
2021-06-07 20:25:05 -0300

    create subscribe api route

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,26 @@
 import styles from './styles.module.scss'
+import { useSession, signIn } from 'next-auth/client'
 
 interface SubscribeButtonProps {
   priceId: string
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      onClick={handleSubscribe}
+      className={styles.subscribeButton}
+    >
       Subscribe now
     </button>
   )
