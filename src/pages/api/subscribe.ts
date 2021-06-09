2a2f7348079d504d146475fab9754d3f878f8d9f
Thalles Ian <thallesyam@users.=.github.com>
2021-06-08 21:05:14 -0300

    create webkkos functions and integration stripe payment

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { query as q } from 'faunadb'
 import { getSession } from 'next-auth/client'
 import { stripe } from '../../services/stripe'
+import { fauna } from '../../services/fauna'
+
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -8,13 +19,31 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       req,
     })
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      // metadata: session.user
-    })
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index('user_by_email'), q.Casefold(session.user.email)))
+    )
+
+    let customerId = user.data.stripe_customer_id
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata: session.user
+      })
+
+      await fauna.query(
+        q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      )
+
+      customerId = stripeCustomer.id
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id, // ID do usuário no painel do stripe
+      customer: customerId, // ID do usuário no painel do stripe
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [{ price: 'price_1IxLIfLIz6rcCXHHMn27Nq0T', quantity: 1 }],
